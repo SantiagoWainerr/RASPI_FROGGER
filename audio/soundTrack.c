b7d77be79fb7d0efb1f8f4f2ba40e35dcbbda201
Santiago Wainer <swainer@itba.edu.ar>
2024-06-30 13:41:13 -0300

    hola

--- audio/soundTrack.c
+++ audio/soundTrack.c
@@ -9,7 +9,9 @@
 #define MUSIC "audio/src/music.wav"
 
 void music (void){
-    playMusic(MUSIC, SDL_MIX_MAXVOLUME/2);
+    while(1){
+        playMusic(MUSIC, SDL_MIX_MAXVOLUME/2);
+    }
 }
 
 void levelSound (void){
